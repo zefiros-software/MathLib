02dea1ae0820bf543916f18cd3213901dfbe2650
KoenVisscher <koen.visscher@hotmail.com>
2015-08-29 15:09:06 +0200

    generic vector 2

--- test/helper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __TEST_HELPER_H__
-#define __TEST_HELPER_H__
-
-#define CONCATEXT( a, b ) a##b
-#define CONCAT( a, b ) CONCATEXT( a, b )
-#define P( prefix ) CONCAT( PREFIX, prefix )
-
-#endif
